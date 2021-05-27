e0c19e5a8df7e8be41eca9a5fb9be4a740a93844
Nate L <Nlarsen23.student@gmail.com>
2021-05-27 03:17:49 +0000

    Added some test comments to exmaple.py to trigger flake8

--- example.py
+++ example.py
@@ -6,7 +6,9 @@ url = os.getenv('HOMEASSISTANT_API_ENDPOINT') # http://localhost:8123/api/
 token = os.getenv('HOMEASSISTANT_TOKEN') # ey816najgfjassf...
 
 
-client = Client(url, token)
+client = Client(url, token) # Creates the object uses to interact with the api
+# In init it checks to see if the api is running by sending a check request
+# If successful it also validates that the configuration.yml on you rhomeasistant is formatted correctly
 
 servicedomains = client.get_services()
 
