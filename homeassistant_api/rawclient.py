f736f48fb2f69b7573b52f882e12232e10be14f8
Gailey Leseman <gaileyleseman@gmail.com>
2021-07-10 21:12:11 +0200

    fix

--- homeassistant_api/rawclient.py
+++ homeassistant_api/rawclient.py
@@ -99,7 +99,7 @@ class RawClient(RawWrapper):
         res = self.request('states')
         return [self._process_entity_json(json) for json in res]
 
-    def get_entity(self, entity_id, attribute="entity_id"):
+    def get_entity(self, entity_id):
         if self.malformed_id(entity_id):
             raise MalformedDataError('"{}" is not a valid entity_id, check your spelling and try again.'.format(entity_id))
         res = self.request(f'states/{entity_id}')
