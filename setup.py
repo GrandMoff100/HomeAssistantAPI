4703ae2d8dda4b94477878dc8ba3ddd241213141
Nate <minecraftcrusher100@gmail.com>
2021-07-13 01:52:49 +0000

    Made needed updates on master vs dev branch to prevent conflicts

--- setup.py
+++ setup.py
@@ -7,11 +7,9 @@ with open('README.md', 'r') as f:
 setup(
     name="HomeAssistant API",
     description="Python Wrapper for Homeassistant's REST API",
-    version='0.1.2',
+    version='2.0.0a1',
     packages=[
         'homeassistant_api',
-        'homeassistant_api.servicedomains',
-        'homeassistant_api.states',
         'homeassistant_api.models'
     ],
     install_requires=['requests'],
