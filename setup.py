c259fc4368195e43233b855027ecdbeb585aeb1c
Nate <minecraftcrusher100@gmail.com>
2021-07-13 01:51:28 +0000

    Merge commit fixing conflicts

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ with open('README.md', 'r') as f:
 setup(
     name="HomeAssistant API",
     description="Python Wrapper for Homeassistant's REST API",
-    version='2.0.0a1',
+    version='0.1.2',
     packages=[
         'homeassistant_api',
+        'homeassistant_api.servicedomains',
+        'homeassistant_api.states',
         'homeassistant_api.models'
     ],
     install_requires=['requests'],
