c07e532c0831524b31f40d570c712798eac008aa
Nate <minecraftcrusher100@gmail.com>
2021-07-14 01:06:14 +0000

    Updated to version 2.0.0 release candidate

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ with open('README.md', 'r') as f:
 setup(
     name="HomeAssistant API",
     description="Python Wrapper for Homeassistant's REST API",
-    version='2.0.0a1',
+    version='2.0.0rc',
     packages=[
         'homeassistant_api',
         'homeassistant_api.models'
     ],
-    install_requires=['requests', 'simplejson'],
+    install_requires=open('requirements.txt', 'r').read().splitlines(),
     long_description=read,
     long_description_content_type='text/markdown'
 )
