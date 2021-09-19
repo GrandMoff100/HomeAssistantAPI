3baff186028f073a9069710ace3719f680116ce8
Nate <minecraftcrusher100@gmail.com>
2021-09-19 02:10:10 +0000

    Update the version to 2.3.0

--- homeassistant_api/__init__.py
+++ homeassistant_api/__init__.py
@@ -2,5 +2,5 @@ from .client import Client, RawClient
 from .models import Group, Entity, Domain, Service, State, Event
 
 
-__version__ = '2.2.0'
+__version__ = '2.3.0'
 __name__ = 'Homeassistant API'
