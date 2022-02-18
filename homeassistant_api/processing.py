7680a97ea8cb1e761045bf9639623ce0b08864c6
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-02-18 05:11:43 +0000

    Added intelligent async/sync processor detection

--- homeassistant_api/processing.py
+++ homeassistant_api/processing.py
@@ -1,66 +1,62 @@
 """Module for processing responses from homeassistant."""
 
+import inspect
 import json
+from typing import Callable, Dict, Tuple
 
+import aiohttp
 import requests
 import simplejson
+from pydantic import BaseModel
 
 from .errors import (
     EndpointNotFoundError,
     MalformedDataError,
     MethodNotAllowedError,
+    ProcessorNotFoundError,
     RequestError,
     UnauthorizedError,
     UnexpectedStatusCodeError,
 )
 
 
-class Processing:
+class Processing(BaseModel):
     """Uses to processor functions to convert json data into common python data types."""
 
     response: requests.Response
-    _processors: dict = {}
-    _async_processors: dict = {}
-
-    def __init__(self, response):
-        self.response = response
+    _processors: Dict[str, Tuple[Callable, ...]] = {}
 
     @staticmethod
-    def processor(mimetype: str, override=False):
+    def processor(mimetype: str):
         """A decorator used to register a response converter function."""
 
         def register_processor(processor):
-            if mimetype not in Processing._processors or override:
-                Processing._processors.update({mimetype: processor})
+            if mimetype not in Processing._processors:
+                Processing._processors[mimetype] = tuple()
+            Processing._processors[mimetype] += (processor,)
             return processor
 
         return register_processor
 
-    @staticmethod
-    def async_processor(mimetype: str, override=False):
-        """A decorator used to register an async response converter function."""
-
-        def register_async_processor(async_processor):
-            if mimetype not in Processing._async_processors or override:
-                Processing._async_processors.update({mimetype: async_processor})
-            return async_processor
-
-        return register_async_processor
-
     def process_content(self, _async: bool):
         """Looks up processors by content-type and then calls the processor with the response."""
-        mimetype = self.response.headers.get("content-type")
+        mimetype = self.response.headers.get("content-type", "text/plain")
+        for processor in self._processors.get(mimetype, ()):
+            if not _async ^ inspect.iscoroutinefunction(processor):
+                return processor(self.response)
         if _async:
-            processor = self._async_processors.get(mimetype, async_process_text)
-        else:
-            processor = self._processors.get(mimetype, process_text)
-        return processor(self.response)
+            raise ProcessorNotFoundError(
+                f"No async response processor registered for mimetype {mimetype!r}"
+            )
+        raise ProcessorNotFoundError(
+            f"No non-async response processor found for mimetype {mimetype!r}"
+        )
 
-    def process(self, _async=False):
+    def process(self):
         """Validates the http status code before starting to process the repsonse content"""
-        if _async:
+        if _async := isinstance(self.response, aiohttp.ClientResponse):
             status_code = self.response.status
-        else:
+        elif _async := not isinstance(self.response, requests.Response):
             status_code = self.response.status_code
         if status_code in (200, 201):
             return self.process_content(_async)
@@ -78,7 +74,8 @@ class Processing:
             "with the request status code and the request content"
         )
         raise UnexpectedStatusCodeError(
-            self.response.status_code, self.response.content
+            self.response.status_code,
+            self.response.content,
         )
 
 
@@ -100,7 +97,7 @@ def process_text(response):
     return response.text
 
 
-@Processing.async_processor("application/json")
+@Processing.processor("application/json")
 async def async_process_json(response):
     """Returns the json dict content of the response."""
     try:
@@ -111,7 +108,7 @@ async def async_process_json(response):
         ) from err
 
 
-@Processing.async_processor("application/octet-stream")
+@Processing.processor("application/octet-stream")
 async def async_process_text(response):
     """Returns the plaintext of the reponse."""
     return await response.text()
