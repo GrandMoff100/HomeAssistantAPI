64f17a4ed3b465b3f9166d4fe53af1235adbfdcd
Nate <minecraftcrusher100@gmail.com>
2022-02-23 21:31:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import os
 
-from homeassistant_api import Client
-
-client = Client(
-    "https://larsen-hassio.duckdns.org:8123/api",
-    os.environ["HOMEASSISTANT_TOKEN"],
-)
-
-
-entries = client.logbook_entries()
-
-for entry in entries:
-    print(entry.entity_id)
