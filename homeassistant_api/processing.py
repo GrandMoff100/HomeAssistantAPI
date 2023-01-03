GrandMoff100/HomeAssistantAPI

commit 3c9e93a054ce9609ede376d95282bbda48755da5

Fix unrelated type error, rather than ignoring.
For some reason, mypy didn't like the typechecking in the ternary operator, but is fine with the statement.

homeassistant_api/processing.py
@@ -93,11 +93,10 @@ class Processing:
         if status_code == 404:
             raise EndpointNotFoundError(str(self._response.url))
         if status_code == 405:
-            method = (
-                self._response.request.method
-                if isinstance(self._response, (Response, CachedResponse))
-                else self._response.method  # type: ignore
-            )
+            if isinstance(self._response, (Response, AsyncCachedResponse)):
+                method = self._response.request.method
+            else:
+                method = self._response.method
             raise MethodNotAllowedError(cast(str, method))
         if status_code >= 500:
             raise InternalServerError(status_code, content)

commit d61b20a5e8a1c288a8562dd6a01583081f41b396

Fix typo introduced in 3c9e93a

homeassistant_api/processing.py
@@ -93,7 +93,7 @@ class Processing:
         if status_code == 404:
             raise EndpointNotFoundError(str(self._response.url))
         if status_code == 405:
-            if isinstance(self._response, (Response, AsyncCachedResponse)):
+            if isinstance(self._response, (Response, CachedResponse)):
                 method = self._response.request.method
             else:
                 method = self._response.method