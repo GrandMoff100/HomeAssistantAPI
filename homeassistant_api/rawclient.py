aff7e230ba0a76a2b2b270d794b4cd4b03e5f5fe
Nathan Larsen <minecraftcrusher100@gmail.com>
2024-03-14 17:06:19 +0000

    Add method endpoints to doc strings

--- homeassistant_api/rawclient.py
+++ homeassistant_api/rawclient.py
@@ -114,11 +114,17 @@ class RawClient(RawBaseClient):
 
     # API information methods
     def get_error_log(self) -> str:
-        """Returns the server error log as a string."""
+        """
+        Returns the server error log as a string.
+        `GET /api/error_log`
+        """
         return cast(str, self.request("error_log"))
 
     def get_config(self) -> Dict[str, Any]:
-        """Returns the yaml configuration of homeassistant."""
+        """
+        Returns the yaml configuration of homeassistant.
+        `GET /api/config`
+        """
         return cast(Dict[str, Any], self.request("config"))
 
     def get_logbook_entries(
@@ -126,7 +132,10 @@ class RawClient(RawBaseClient):
         *args,
         **kwargs,
     ) -> Generator[LogbookEntry, None, None]:
-        """Returns a list of logbook entries from homeassistant."""
+        """
+        Returns a list of logbook entries from homeassistant.
+        `GET /api/logbook/<timestamp>
+        """
         params, url = self.prepare_get_logbook_entry_params(*args, **kwargs)
         data = self.request(url, params=params)
         for entry in data:
@@ -142,6 +151,7 @@ class RawClient(RawBaseClient):
     ) -> Generator[History, None, None]:
         """
         Yields entity state histories. See docs on the `History` model.
+        `GET /api/history/period/<timestamp>`
         """
         params, url = self.prepare_get_entity_histories_params(
             entities=entities,
@@ -160,6 +170,7 @@ class RawClient(RawBaseClient):
         """
         Renders a Jinja2 template with Home Assistant context data.
         See https://www.home-assistant.io/docs/configuration/templating.
+        `POST /api/template`
         """
         try:
             return cast(
@@ -185,7 +196,10 @@ class RawClient(RawBaseClient):
 
     # API check methods
     def check_api_config(self) -> bool:
-        """Asks Home Assistant to validate its configuration file."""
+        """
+        Asks Home Assistant to validate its configuration file.
+        `POST /api/config/core/check_config`
+        """
         res = cast(
             Dict[str, Any], self.request("config/core/check_config", method="POST")
         )
@@ -193,13 +207,19 @@ class RawClient(RawBaseClient):
         return valid
 
     def check_api_running(self) -> bool:
-        """Asks Home Assistant if it is running."""
+        """
+        Asks Home Assistant if it is running.
+        `GET /api/`
+        """
         res = self.request("")
         return cast(Dict[str, Any], res).get("message") == "API running."
 
     # Entity methods
     def get_entities(self) -> Dict[str, Group]:
-        """Fetches all entities from the api"""
+        """
+        Fetches all entities from the api and returns them as a dictionary of :py:class:`Group`'s.
+        `GET /api/states`
+        """
         entities: Dict[str, Group] = {}
         for state in self.get_states():
             group_id, entity_slug = state.entity_id.split(".")
@@ -217,7 +237,10 @@ class RawClient(RawBaseClient):
         slug: str | None = None,
         entity_id: str | None = None,
     ) -> Optional[Entity]:
-        """Returns an :py:class:`Entity` model for an :code:`entity_id`"""
+        """
+        Returns an :py:class:`Entity` model for an :code:`entity_id`.
+        `GET /api/states/<entity_id>`
+        """
         if group_id is not None and slug is not None:
             state = self.get_state(group_id=group_id, slug=slug)
         elif entity_id is not None:
@@ -240,7 +263,10 @@ class RawClient(RawBaseClient):
 
     # Services and domain methods
     def get_domains(self) -> Dict[str, Domain]:
-        """Fetches all :py:class:`Service` 's from the API."""
+        """
+        Fetches all :py:class:`Service` 's from the API.
+        `GET /api/services`
+        """
         data = self.request("services")
         domains = map(
             lambda json: Domain.from_json(json, client=cast(Client, self)),
@@ -249,7 +275,10 @@ class RawClient(RawBaseClient):
         return {domain.domain_id: domain for domain in domains}
 
     def get_domain(self, domain_id: str) -> Optional[Domain]:
-        """Fetches all :py:class:`Service`'s under a particular service :py:class:`Domain`."""
+        """
+        Fetches all :py:class:`Service`'s under a particular service :py:class:`Domain`.
+        Uses cached data from :py:meth:`get_domains` if available.
+        """
         return self.get_domains().get(domain_id)
 
     def trigger_service(
@@ -258,7 +287,10 @@ class RawClient(RawBaseClient):
         service: str,
         **service_data,
     ) -> Tuple[State, ...]:
-        """Tells Home Assistant to trigger a service, returns all states changed while in the process of being called."""
+        """
+        Tells Home Assistant to trigger a service, returns all states changed while in the process of being called.
+        `POST /api/services/<domain>/<service>`
+        """
         data = self.request(
             join("services", domain, service),
             method="POST",
@@ -274,7 +306,10 @@ class RawClient(RawBaseClient):
         group_id: Optional[str] = None,
         slug: Optional[str] = None,
     ) -> State:
-        """Fetches the state of the entity specified"""
+        """
+        Fetches the state of the entity specified.
+        `GET /api/states/<entity_id>`
+        """
         entity_id = self.prepare_entity_id(
             group_id=group_id,
             slug=slug,
@@ -289,7 +324,8 @@ class RawClient(RawBaseClient):
     ) -> State:
         """
         This method sets the representation of a device within Home Assistant and will not communicate with the actual device.
-        To communicate with the device, use :py:meth:`Service.trigger` or :py:meth:`Service.async_trigger`
+        To communicate with the device, use :py:meth:`Service.trigger` or :py:meth:`Service.async_trigger`.
+        `POST /api/states/<entity_id>`
         """
         data = self.request(
             join("states", state.entity_id),
@@ -299,14 +335,20 @@ class RawClient(RawBaseClient):
         return State.from_json(cast(Dict[str, Any], data))
 
     def get_states(self) -> Tuple[State, ...]:
-        """Gets the states of all entities within homeassistant"""
+        """
+        Gets the states of all entities within homeassistant.
+        `GET /api/states`
+        """
         data = self.request("states")
         states = map(State.from_json, cast(List[Dict[str, Any]], data))
         return tuple(states)
 
     # Event methods
     def get_events(self) -> Tuple[Event, ...]:
-        """Gets the Events that happen within homeassistant"""
+        """
+        Gets the Events that happen within homeassistant
+        `GET /api/events`
+        """
         data = self.request("events")
         return tuple(
             map(
@@ -316,14 +358,20 @@ class RawClient(RawBaseClient):
         )
 
     def get_event(self, name: str) -> Optional[Event]:
-        """Gets the :py:class:`Event` with the specified name if it has at least one listener."""
+        """
+        Gets the :py:class:`Event` with the specified name if it has at least one listener.
+        Uses cached data from :py:meth:`get_events` if available.
+        """
         for event in self.get_events():
             if event.event == name.strip().lower():
                 return event
         return None
 
     def fire_event(self, event_type: str, **event_data) -> Optional[str]:
-        """Fires a given event_type within homeassistant. Must be an existing event_type."""
+        """
+        Fires a given event_type within homeassistant. Must be an existing event_type.
+        `POST /api/events/<event_type>`
+        """
         data = self.request(
             join("events", event_type),
             method="POST",
@@ -332,5 +380,8 @@ class RawClient(RawBaseClient):
         return cast(dict[str, Any], data).get("message")
 
     def get_components(self) -> Tuple[str, ...]:
-        """Returns a tuple of all registered components."""
+        """
+        Returns a tuple of all registered components.
+        `GET /api/components`
+        """
         return tuple(self.request("components"))
